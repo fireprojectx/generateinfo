53940fdef259d1384e8ad7388d1605b2b7517b02
Wallace <167040540+fireprojectx@users.noreply.github.com>
2024-08-09 01:58:52 +0000

    carregando imagens página inicial

--- total-english-app/src/App.js
+++ total-english-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="progress-bar">
+        <div className="progress-bar-inner"></div>
+      </div>
+      <img src="/logo.png" alt="Total English" className="logo"/>
+      <h1>Plan de Aprendizaje Acelerado</h1>
+      <p>Seleccione su género para continuar. El cuestionario toma unos pocos minutos.</p>
+      <div className="gender-selection">
+        <div className="gender-option" onClick={() => alert('Hombre seleccionado')}>
+          <img src="/hombre.png" alt="Hombre" className="gender-img"/>
+          <p>Hombre</p>
+        </div>
+        <div className="gender-option" onClick={() => alert('Mujer seleccionada')}>
+          <img src="/mujer.png" alt="Mujer" className="gender-img"/>
+          <p>Mujer</p>
+        </div>
+      </div>
+      <p className="terms">
+        Al hacer clic en alguna de las opciones, usted acepta los <a href="#">Términos de uso y servicio</a>, <a href="#">Política de privacidad</a>, <a href="#">Política de suscripción</a> y <a href="#">Política de cookies</a>.
+      </p>
     </div>
   );
 }
 
 export default App;
+
